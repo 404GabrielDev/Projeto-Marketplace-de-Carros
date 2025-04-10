78a776b9641627e8aef61a3483708646f749853c
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-10 10:44:35 -0300

    feat: adicionar controller com rota de registro

---
+++ src/main/java/com/service/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package com.service.controllers;
+
+import com.service.dto.SignupRequest;
+import com.service.dto.UserDTO;
+import com.service.services.auth.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+        if(authService.hasUserWithEmail(signupRequest.getEmail())) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("User Already exist");
+        }
+
+        UserDTO userDTO = authService.signup(signupRequest);
+
+        if(userDTO == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDTO);
+    }
+}
