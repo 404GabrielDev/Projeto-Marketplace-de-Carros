14c2ceca72d9ab2c1f9c2c97244222225e15a753
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-23 23:21:16 -0300

    feat: implementar componente registro

---
+++ src/app/auth/auth-components/signup/signup.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    ReactiveFormsModule,
+    NzSpinModule,
+    NzFormModule,
+    NzInputModule,
+    NzIconModule,
+    NzButtonModule,
+    RouterModule
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {
+  signupForm!: FormGroup;
+
+  get name() {
+    return this.signupForm.get('name')!;
+  }
+
+  get email() {
+    return this.signupForm.get('email')!;
+  }
+
+  get password() {
+    return this.signupForm.get('password')!;
+  }
+
+  get confirmPassword() {
+    return this.signupForm.get('passwordConfirm')!;
+  }
+
+  isSpinning: boolean = false;
+
+  constructor(private fb: FormBuilder, private service:AuthService, private router: Router) {
+    this.signupForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      confirmPassword: [null, [Validators.required]],
+    });
+  }
+
+  //entender validação personalizada
+
+  confirmationValidator = (control:FormControl): {[s:string]:boolean } => {
+    if(!control.value)
+      return{require: true};
+    else if(control.value !== this.signupForm.controls["password"].value)
+      return{confirm: true, error:true}
+    return{};
+  }
+
+  signup() {
+    console.log(this.signupForm.value);
+    this.service.register(this.signupForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigate(['/login'])
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
+}
