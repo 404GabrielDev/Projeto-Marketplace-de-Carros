b458ef4d5b7850e45bac2df13ace0b9fb0824b0e
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-05-30 18:32:20 -0300

    feat: creating a component admin and routing

--- src/app/auth/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/auth/modules/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../services/admin.service';
+import { CommonModule } from '@angular/common';
+
+
+
+interface Car {
+  id: number;
+  name: string;
+  brand: string;
+  type: string;
+  transmission: string;
+  color: string;
+  description: string;
+  price: number;
+  img : File | null;
+  returnedImg: string;
+  year: string;
+
+  //RESOLVER PROBLEMA DE DIFERENTES TAMANHOS DE IMAGENS
+  imageOrientation?:'horizontal' | 'vertical';
+}
+
+
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
 export class AdminDashboardComponent {
+  cars:Car[] = []
+
+  constructor(private service:AdminService) {}
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+
+  getCars() {
+    this.service.getAllCars().subscribe((res) => {
+      this.cars = res
+
+      this.cars.forEach((car) => {
+        const img = new Image();
+        img.src = 'data:image/jpeg;base64,' + car.returnedImg;
+
+        img.onload = () => {
+          car.imageOrientation = img.width > img.height ? 'horizontal' : 'vertical';
+
+          img.onerror = (err) => {
+            console.error('Erro ao carregar a imagem', err)
+          }
+        }
+      })
+    })
+  }
+
 
 }
--- src/app/auth/modules/customer/services/customer.service.ts
+++ src/app/auth/modules/customer/services/customer.service.ts
@@ -1,9 +1,96 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../../services/auth/auth.service';
+import { throwError } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+const BASE_URL = "http://localhost:8080/"
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService:AuthService) { }
+
+  postCar(formData: FormData): Observable<any> {
+    return this.http.post(BASE_URL + 'api/customer/car', formData, {
+      withCredentials: true,
+    });
+  }
+
+  getAllCars(): Observable<any> {
+    return this.http.get(BASE_URL + 'api/customer/cars', {
+      withCredentials: true,
+    });
+  }
+
+  getMyCars(): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/my-cars/${this.authService.getUserId()}`, {
+      withCredentials: true,
+    });
+  }
+
+  getMyBids(): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/bids/${this.authService.getUserId()}`, {
+      withCredentials: true,
+    });
+  }
+
+  getBidsByCarId(carId:number): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/${carId}/bids`, {
+      withCredentials: true,
+    });
+  }
+
+  getAnalytics(): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/analytics/${this.authService.getUserId()}`, {
+      withCredentials: true,
+    });
+  }
+
+
+
+  updateBidStatus(bidId:number, status:string): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/bid/${bidId}/${status}`, {
+      withCredentials: true,
+    });
+  }
+
+  deleteCar(id:number): Observable<any> {
+    return this.http.delete(BASE_URL + `api/customer/deletecar/${id}`, {
+      withCredentials: true,
+    });
+  }
+
+   getCarById(id:number): Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/${id}`, {
+      withCredentials: true,
+    });
+  }
+
+  updateCar(id:number, formData:FormData): Observable<any> {
+    return this.http.put(BASE_URL + `api/customer/car/${id}`, formData, {
+      withCredentials: true,
+    });
+  }
+
+  bidACar(bid: {price: string, userId: string, carId: number}):Observable<any> {
+    return this.http.post(BASE_URL + 'api/customer/car/bid', bid, {
+      withCredentials: true,
+    })
+  }
+
+  searchCar(searchDto:any):Observable<any> {
+    return this.http.post(BASE_URL + 'api/customer/car/search', searchDto, {
+      withCredentials: true,
+    })
+  }
+
+  getBids(id:string):Observable<any> {
+    return this.http.get(BASE_URL + `api/customer/car/bids/${id}`, {
+      withCredentials: true
+    })
+  }
 }
