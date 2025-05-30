f0381d603c330eeebfeb49454b9d4cf5647b5371
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-05-30 19:03:00 -0300

    feat: implementar dto do componente SearchCar

---
+++ src/main/java/com/service/dto/SearchCarDTO.java
@@ -0,0 +1,15 @@
+package com.service.dto;
+
+import lombok.Data;
+
+@Data
+public class SearchCarDTO {
+
+    private String brand;
+
+    private String type;
+
+    private String color;
+
+    private String transmission;
+}
